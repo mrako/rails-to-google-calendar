8b694518095c327dab55d9dedebd3d29332ab691
Marko Klemetti <marko.klemetti@gmail.com>
2015-06-06 11:08:10 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 
-Meeting.create(title: "Esimerkkitapahtuma", start_time: Time.now, end_time: Time.now+8.hours, description: "Kuvaus esimerkkitapahtumalle"
-Meeting.create(title: "Toinen esimerkkitapahtuma", start_time: Time.now+1.day, end_time: Time.now+4.days, description: "Pidempi kuvaus esimerkkitapahtumalle"
+Meeting.create(title: "Esimerkkitapahtuma", start_time: Time.now, end_time: Time.now+8.hours, description: "Kuvaus esimerkkitapahtumalle")
+Meeting.create(title: "Toinen esimerkkitapahtuma", start_time: Time.now+1.day, end_time: Time.now+4.days, description: "Pidempi kuvaus esimerkkitapahtumalle")
