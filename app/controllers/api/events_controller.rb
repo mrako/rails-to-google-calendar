ace450772b63cd2d94548faf8423af60b40c8f82
Marko Klemetti <marko.klemetti@gmail.com>
2015-06-06 16:03:12 +0300

    Update dates to see update interval

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,8 +2,8 @@ class Api::EventsController < ApplicationController
   include ActionController::MimeResponds
 
   def index
-    date1 = DateTime.new(2015, 6, 4, 9, 0, 0)
-    date2 = DateTime.new(2015, 6, 8, 12, 0, 0)
+    date1 = DateTime.new(2015, 6, 7, 11, 0, 0)
+    date2 = DateTime.new(2015, 6, 10, 10, 0, 0)
 
     events = [Event.new("First reservation (less than 8 hours)", date1, date1+5.hours, "The changed fake event location", "Description of the reservation"),
               Event.new("title2", date2, date2+7.days)]
